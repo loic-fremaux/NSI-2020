153f282aab7d0c0330bfea3cec9dc9dcc70a3338
Loïc Frémaux <loic.fremaux1@gmail.com>
2020-05-06 19:50:04 +0200

    Add 0-3

---
+++ Sequence0/3. Min and Max.py
@@ -0,0 +1,20 @@
+def minimum(list):
+    return extract(list, lambda r, v: r > v)
+
+
+def maximum(list):
+    return extract(list, lambda r, v: r < v)
+
+
+def extract(list, comparator):
+    result = list[0]
+    for i in range(len(list)):
+        if comparator(result, list[i]):
+            result = list[i]
+    return result
+
+
+# Tests
+tab = [4, 3, 9, 7, 11, 5, 4, 8, 9, 0, 1]
+print("Smallest number is: ", minimum(tab))
+print("Biggest number is: ", maximum(tab))
