005b3443b569295b00794678285c472f9780971f
Loïc Frémaux <loic.fremaux1@gmail.com>
2020-05-06 19:42:40 +0200

    Add 0-2

---
+++ Sequence0/2. Occurences with for loop.py
@@ -0,0 +1,16 @@
+def find(elt, tab):
+    for i in range(0, len(tab)):
+        if elt == tab[i]:
+            return i
+    return -1
+
+
+# Tests
+print(find(
+    "2",
+    ["un", "2", 3, "Quatre", 5]
+))
+print(find(
+    2,
+    ["un", "2", 3, "Quatre", 5]
+))
