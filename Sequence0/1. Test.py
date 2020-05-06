8f4c0260c6f974b0a5d6eeb98b7f581abfaaafb4
Loïc Frémaux <loic.fremaux1@gmail.com>
2020-05-06 19:40:02 +0200

    Add 0-1

---
+++ Sequence0/1. Test.py
@@ -0,0 +1,20 @@
+def find(elt, tab):
+    n = len(tab)
+    i = 0
+    while i < n and elt != tab[i]:
+        i += 1
+    if i == n:
+        return -1
+    else:
+        return i
+
+
+# Tests
+print(find(
+    "2",
+    ["un", "2", 3, "Quatre", 5]
+))
+print(find(
+    2,
+    ["un", "2", 3, "Quatre", 5]
+))
