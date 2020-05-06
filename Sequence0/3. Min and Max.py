4ad7941903b802262909cb42fb799518cffb1e9f
Loïc Frémaux <loic.fremaux1@gmail.com>
2020-05-06 20:53:34 +0200

    Refactoring

--- Sequence0/3. Min and Max.py
+++ Sequence0/3. Min and Max.py
@@ -1,20 +1,20 @@
-def minimum(list):
-    return extract(list, lambda r, v: r > v)
+def minimum(tab):
+    return extract(tab, lambda r, v: r > v)
 
 
-def maximum(list):
-    return extract(list, lambda r, v: r < v)
+def maximum(tab):
+    return extract(tab, lambda r, v: r < v)
 
 
-def extract(list, comparator):
-    result = list[0]
-    for i in range(len(list)):
-        if comparator(result, list[i]):
-            result = list[i]
+def extract(tab, comparator):
+    result = tab[0]
+    for i in range(len(tab)):
+        if comparator(result, tab[i]):
+            result = tab[i]
     return result
 
 
 #  Tests
-tab = [4, 3, 9, 7, 11, 5, 4, 8, 9, 0, 1]
-print("Smallest number is: ", minimum(tab))
-print("Biggest number is: ", maximum(tab))
+test = [4, 3, 9, 7, 11, 5, 4, 8, 9, 0, 1]
+print("Smallest number is: ", minimum(test))
+print("Biggest number is: ", maximum(test))
